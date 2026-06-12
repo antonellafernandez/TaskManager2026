4dc0f32a24cf54a32843d2d55fbd11c44b7f9a84
Antonella Fernández <antonellafernandez1704@gmail.com>
2026-06-12 19:25:08 -0300

    feat(tasks): enhance task filters styling and interaction

--- src/components/TaskFilters.tsx
+++ src/components/TaskFilters.tsx
@@ -10,31 +10,32 @@ export default function TaskFilters({
   onChange,
 }: Props) {
   return (
-    <div className="mb-4 flex gap-2">
+    <div className="mb-4 flex gap-4 text-slate-950">
       <button
         onClick={() => onChange("ALL")}
-        className={status === "ALL" ? "font-bold" : ""}
+        className={
+          status === "ALL" ? "font-bold" : "cursor-pointer"}
       >
         All
       </button>
 
       <button
         onClick={() => onChange("pending")}
-        className={status === "pending" ? "font-bold" : ""}
+        className={status === "pending" ? "font-bold text-pink-500" : "cursor-pointer"}
       >
         Pending
       </button>
 
       <button
         onClick={() => onChange("in-progress")}
-        className={status === "in-progress" ? "font-bold" : ""}
+        className={status === "in-progress" ? "font-bold text-blue-500" : "cursor-pointer"}
       >
         In Progress
       </button>
 
       <button
         onClick={() => onChange("completed")}
-        className={status === "completed" ? "font-bold" : ""}
+        className={status === "completed" ? "font-bold text-lime-500" : "cursor-pointer"}
       >
         Completed
       </button>
